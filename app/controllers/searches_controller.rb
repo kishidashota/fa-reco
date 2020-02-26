b5244b646ebc5ad3afb57841fe55f8264b05afae
kishidashota <shidanemoaki@gmail.com>
2020-02-26 18:33:26 +0900

    name&seach

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,6 +3,14 @@ class SearchesController < ApplicationController
   def index
     @products = Product.search(params[:search]).limit(10)
     @search = params[:search]
+    if user_signed_in?
+      @shops = Shop.all
+      rooms = current_user.rooms
+      #自分が入ってるroomの相手のidを格納する
+      @shop_ids = []
+      rooms.each do |r|
+        @shop_ids << r.shop_id
+      end
+    end
   end
-  
 end
