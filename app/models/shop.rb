a69928dcee4831be7cf0c7ef2211891cbfb40157
kishidashota <shidanemoaki@gmail.com>
2020-03-03 00:35:26 +0900

    image

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -3,7 +3,7 @@ class Shop < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  mount_uploader :shop_image,imageUploader
+  mount_uploader :shop_image,ImageUploader
   has_many :rooms
   has_many :products, dependent: :destroy
   validates :name, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  mount_uploader :user_image, imageUploader
+  mount_uploader :user_image, ImageUploader
   validates :profile, length: { maximum: 200 } 
   has_many :rooms
 end
