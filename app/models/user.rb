e46396976418982b6f7e307cfdc71f349b64bfd4
kishidashota <shidanemoaki@gmail.com>
2020-03-03 00:06:01 +0900

    image 9

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  mount_uploader :image, ImageUploader
+  mount_uploader :user_image, User_imageUploader
   validates :profile, length: { maximum: 200 } 
   has_many :rooms
 end
