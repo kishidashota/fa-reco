7e3b593e41e7143190d74563bac4b05f88fd046f
kishidashota <shidanemoaki@gmail.com>
2020-03-02 17:17:27 +0900

    商品出品　バリデーション

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ActiveRecord::Base
   mount_uploader :image, ImageUploader
   belongs_to :shop
+  validates :name, :price,:image , presence: true
   def self.search(search) #self.でクラスメソッドとしている
     if search # Controllerから渡されたパラメータが!= nilの場合は、titleカラムを部分一致検索
       Product.where(['name LIKE ?', "%#{search}%"])
